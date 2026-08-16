884226b3428c356c8d727a095b5d9b977d3ec8b6
Luciano Russica <russica@hotmail.de>
2026-08-16 16:55:15 +0200

    Initial Commit

---
+++ LabPlate/LabPlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabPlateApp.swift
+//  LabPlate
+//
+//  Created by unitoflove on 16.08.26.
+//
+
+import SwiftUI
+
+@main
+struct LabPlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
